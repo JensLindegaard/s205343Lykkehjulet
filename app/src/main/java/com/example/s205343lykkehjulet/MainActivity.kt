ee07fb10de9f19b9bb5249d8e5aec7aa63f65b2a
Jens Valdemar Lindegaard <s205343@student.dtu.dk>
2021-11-27 12:44:59 +0100

    git

---
+++ app/src/main/java/com/example/s205343lykkehjulet/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.s205343lykkehjulet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
