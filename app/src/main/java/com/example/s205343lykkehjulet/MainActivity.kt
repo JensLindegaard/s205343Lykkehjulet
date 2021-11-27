c14d975dcc8f16dd6e07a2be38635b6b8b52581f
Jens Valdemar Lindegaard <s205343@student.dtu.dk>
2021-11-27 12:48:13 +0100

    TEST

--- app/src/main/java/com/example/s205343lykkehjulet/MainActivity.kt
+++ app/src/main/java/com/example/s205343lykkehjulet/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        println("TEST")
     }
 }
